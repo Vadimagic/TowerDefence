51a068eb505721e8f9096025e2f56364e946c42c
Vadim <vadim.prog.2000@gmail.com>
2021-03-01 23:59:39 +0300

    add comments for js code

---
+++ script.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 900;
+canvas.height = 600;
+
+// global variables
+
+// game board
+
+// projectiles
+
+// defenders
+
+// enemies
+
+// resources
+
+// utilities
