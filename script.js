400a068ef60f52b124258fc07224d4ed87bffc6c
Vadim <vadim.prog.2000@gmail.com>
2021-03-14 23:59:30 +0300

    add function handle floating messages

--- script.js
+++ script.js
@@ -183,10 +183,46 @@ function handleDefenders() {
         i--;
         enemies[j].movement = enemies[j].speed
       }
-    }
+    } 
   }
 }
 
+// floating messages
+const floatingMessages = [];
+class floatingMessage {
+  constructor(value, x, y, color) {
+    this.value = value;
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.lifeSpan = 0;
+    this.color = color;
+    this.opacity = 1;
+  }
+  update() {
+    this.y -= 0.3;
+    this.lifeSpan += 1;
+    if (this.opacity > 0.01) this.opacity -= 0.01;
+  }
+  draw() {
+    ctx.globalAlpha = this.opacity;
+    ctx.fillStyle = this.color;
+    ctx.font = this.size + 'px Orbitron';
+    ctx.fillText(this.value, this.x, this.y)
+    ctx.globalAlpha = 1;
+  }
+}
+
+function handleFloatingMessage() {
+  for (let i = 0; i < floatingMessages.length; i++) {
+    floatingMessages[i].update();
+    floatingMessages[i].draw();
+    if (floatingMessages[i].lifeSpan >= 50) {
+      floatingMessages.splice(i, 1);
+      i--;
+    }
+  }
+}
 
 // enemies
 class Emeny {
